016c3ea031dd13737aaf60ab191d95163b840730
Sloth404 <malte.moehring@protonmail.com>
2024-08-11 12:02:06 +0200

    Initial commit

---
+++ src/lib/sundspace-website.spec.ts
@@ -0,0 +1,7 @@
+import { sundspaceWebsite } from './sundspace-website';
+
+describe('sundspaceWebsite', () => {
+  it('should work', () => {
+    expect(sundspaceWebsite()).toEqual('sundspace-website');
+  });
+});
---
+++ src/lib/sundspace-website.ts
@@ -0,0 +1,3 @@
+export function sundspaceWebsite(): string {
+  return 'sundspace-website';
+}
