217a07eb9abe4a5fc26b6612692f18c788453eed
Sloth404 <malte.moehring@protonmail.com>
2024-08-14 18:04:31 +0200

    styling of cards

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,23 +6,33 @@
       </div>
       <div class="feed-1">
         <img :src="getImagePath(feeds[0].image)" alt="Feed 1" />
-        <p>{{ feeds[0].text }}</p>
+        <div class="card yellow">
+          <p>{{ feeds[0].text }}</p>
+        </div>
       </div>
       <div class="feed-2">
         <img :src="getImagePath(feeds[1].image)" alt="Feed 2" />
-        <p>{{ feeds[1].text }}</p>
+        <div class="card purple">
+          <p>{{ feeds[1].text }}</p>
+        </div>
       </div>
       <div class="feed-3">
         <img :src="getImagePath(feeds[2].image)" alt="Feed 3" />
-        <p>{{ feeds[2].text }}</p>
+        <div class="card yellow">
+          <p>{{ feeds[2].text }}</p>
+        </div>
       </div>
       <div class="feed-4">
         <img :src="getImagePath(feeds[3].image)" alt="Feed 4" />
-        <p>{{ feeds[3].text }}</p>
+        <div class="card purple">
+          <p>{{ feeds[3].text }}</p>
+        </div>
       </div>
       <div class="feed-5">
         <img :src="getImagePath(feeds[4].image)" alt="Feed 5" />
-        <p>{{ feeds[4].text }}</p>
+        <div class="card yellow">
+          <p>{{ feeds[4].text }}</p>
+        </div>
       </div>
       <div class="footer">7</div>
       <div class="rocket-element">8</div>
@@ -45,13 +55,17 @@ const getImagePath = (imageName: string) => {
 
 <style scoped lang="css">
 img {
-  max-width: 80%;
+  max-width: 80%; /* Bild füllt den verfügbaren Platz in der Karte */
   max-height: 80%;
-  padding-left: 10rem;
+  border-radius: 20px; /* Rundet alle Ecken des Bildes */
+  object-fit: cover; /* Das Bild wird so angepasst, dass es den Container ausfüllt */
+  z-index: 1;
+  position: relative;
 }
 
 p {
-  padding-left: 15rem;
+  padding: 20px;
+  color:black
 }
 
 .parent {
@@ -74,32 +88,43 @@ p {
 }
 
 .feed-1 {
+  position: relative;
   grid-column: span 2 / span 2;
   grid-row-start: 2;
+  margin-left: 10rem;
 }
 
 .feed-2 {
+  position: relative;
   grid-column: span 2 / span 2;
   grid-column-start: 1;
   grid-row-start: 3;
+  margin-left: 10rem;
 }
 
 .feed-3 {
+  position: relative;
   grid-column: span 2 / span 2;
   grid-column-start: 1;
   grid-row-start: 4;
+  margin-left: 10rem;
 }
 
 .feed-4 {
+  position: relative;
   grid-column: span 2 / span 2;
   grid-column-start: 1;
   grid-row-start: 5;
+  margin-left: 10rem;
+  padding-bottom: auto;
 }
 
 .feed-5 {
+  position: relative;
   grid-column: span 2 / span 2;
   grid-column-start: 1;
   grid-row-start: 6;
+  margin-left: 10rem;
 }
 
 .footer {
@@ -112,5 +137,23 @@ p {
   grid-row: span 5 / span 5;
   grid-column-start: 3;
   grid-row-start: 2;
+}
+
+.card {
+  position: absolute;
+  border-radius: 20px;
+  padding-right: 20px; /* Optionales Padding rechts für Symmetrie */
+  margin-left: 5rem; /* Setzt den linken Rand zurück */
+  width: calc(100% - 10rem); /* Breite des Bildes anpassen, um das Padding zu berücksichtigen */
+  margin-top: -5rem;
+  z-index: 999;
+}
+
+.yellow {
+  background-color: #fddb3a;
+}
+
+.purple {
+  background-color: blueviolet;
 }
 </style>
