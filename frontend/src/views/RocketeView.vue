d252eba5d59b3975c772620b6a40931ef740e18a
Sloth404 <malte.moehring@protonmail.com>
2024-08-21 19:58:58 +0200

    RocketView granular information about some parts added, info defining line added

--- frontend/src/views/RocketeView.vue
+++ frontend/src/views/RocketeView.vue
@@ -11,17 +11,27 @@ Wie erhoffen uns mit unserer Rakete nicht nur Einblicke in die Luft- und Raumfah
     </div>
     <div class="parent">
       <div class="parachute">
-
+        <h2>Fallschirm</h2>
+        <div class="line"></div>
+        <p>Das Fallschirmsystem unserer Rakete, die für den EUROC-Wettbewerb entwickelt wurde, spielt eine entscheidende Rolle bei der sicheren Rückführung der Nutzlast. Nach dem Erreichen der geplanten Flughöhe von 3000 Metern wird das zweistufige Fallschirmsystem ausgelöst. Zunächst sorgt ein kleinerer Drogue-Fallschirm für die Stabilisierung und Verlangsamung des Abstiegs. Auf einer Höhe von etwa 500 Metern öffnet sich dann der Hauptfallschirm, um eine sanfte Landung der Rakete zu gewährleisten. Das gesamte System wurde sorgfältig berechnet und getestet, um sowohl die Strukturintegrität der Rakete als auch die Sicherheit der Nutzlast zu gewährleisten.</p>
       </div>
       <div class="electronics">
+        <h2>Elektronik</h2>
+        <div class="line"></div>
+        <p>Die On-Board-Elektronik unserer Rakete, die beim EUROC-Wettbewerb eingesetzt wird, ist ein zentraler Bestandteil für die erfolgreiche Mission. Sie umfasst ein Flight-Computer-System, das für die Steuerung und Überwachung aller kritischen Prozesse verantwortlich ist. Dazu gehören die Erfassung und Aufzeichnung von Flugdaten, die Auslösung des Fallschirmsystems in den richtigen Phasen sowie die Aktivierung von Sicherheitssystemen im Notfall.
 
+          Zudem sind verschiedene Sensoren wie Höhenmesser, Beschleunigungssensoren und GPS-Module integriert, um präzise Echtzeit-Daten zu liefern. Diese Informationen werden sowohl für die Steuerung während des Flugs als auch für die spätere Analyse genutzt. Die Elektronik wurde speziell entwickelt und programmiert, um unter den extremen Bedingungen eines Raketenflugs zuverlässig zu funktionieren.</p>
       </div>
       <div class="engine">
+        <h2>Triebwerk</h2>
+        <div class="line"></div>
+        <p>Das Triebwerk unserer Rakete, entwickelt für den EUROC-Wettbewerb, ist das Herzstück der Mission. Es handelt sich um ein Feststofftriebwerk, das sorgfältig ausgewählt und optimiert wurde, um die Rakete auf die geplante Höhe von 3000 Metern zu bringen. Das Triebwerk liefert den notwendigen Schub, um die Rakete sicher durch die verschiedenen Phasen des Flugs zu bringen, vom Start bis zum Erreichen der Maximalhöhe.
 
+          Besonderes Augenmerk wurde auf die thermische Stabilität und die effiziente Verbrennung des Treibstoffs gelegt, um eine gleichmäßige Beschleunigung und optimale Flugbahn zu gewährleisten. Das Design des Triebwerks und die Auswahl der Materialien wurden intensiv getestet, um den extremen Bedingungen standzuhalten und gleichzeitig die Sicherheit des gesamten Systems zu gewährleisten. Zudem wurde das Triebwerk so konstruiert, dass es sich nahtlos in die gesamte Raketenstruktur integriert und maximale Leistung bei minimalem Gewicht bietet.</p>
       </div>
-    </div>
-    <div class="rocket-element">
-      <Rocket></Rocket>
+      <div class="rocket-element">
+        <Rocket></Rocket>
+      </div>
     </div>
   </main>
 </template>
@@ -61,19 +71,62 @@ import Rocket from '../components/RocketComponent.vue';
 .parent {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(7, auto);
+  grid-template-rows: repeat(3, 1fr);
   gap: 8px;
+  height: auto;
+}
+
+.parent div h2 {
+  font-weight: bold;
+  text-transform: uppercase;
+  color:#fddb3a;
+  padding-left: 5rem;
+  font-size: 24px;
+}
+
+.parent div p {
+  color: #dadada;
+  padding-left: 7.5rem;
+  padding-top: 1.5rem;
+  padding-right: 5rem;
+}
+
+.parachute {
+  grid-column: span 2 / span 2;
+}
+
+.electronics {
+  grid-column: span 2 / span 2;
+  grid-column-start: 1;
+  grid-row-start: 2;
+}
+
+.engine {
+  grid-column: span 2 / span 2;
+  grid-column-start: 1;
+  grid-row-start: 3;
 }
 
 .rocket-element {
+  grid-row: span 3/ span 3;
+  grid-column-start: 3;
+  grid-row-start: 1;
   width: 100%;
-  height: 100vh;
   max-width: 25vw;
-  z-index: 0;
+  z-index: 1;
   overflow: hidden;
+}
+
+.line {
+  position: relative;
+  left: 10vw;
+  height: 2px;
+  background-color: #fddb3a;
+  z-index: 2;
 }
 
 Rocket {
   height: 100%;
+  overflow: hidden;
 }
 </style>
